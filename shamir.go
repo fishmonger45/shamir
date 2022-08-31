c2c96ea0ca7ebf932cdefe6e7543fa4fcf5585eb
wqsz7xn <wqsz7xn@protonmail.com>
2022-08-31 22:56:05 +1200

    terrible

--- shamir.go
+++ shamir.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"errors"
-	"math"
+	"fmt"
 	"math/big"
 	"math/rand"
+	"os"
 )
 
 var (
@@ -13,8 +14,8 @@ var (
 )
 
 type Share struct {
-	Part  *big.Int
-	Piece int
+	Y *big.Int
+	X int
 }
 
 type Polynomial struct {
@@ -37,6 +38,29 @@ func NewPolynomial(required int) (*Polynomial, error) {
 	}, nil
 }
 
+func (polynomial *Polynomial) Evaluate(x int64) *big.Int {
+	acc := new(big.Int)
+	acc.Set(polynomial.Coefficients[0])
+	fmt.Printf("acc: %v\n", acc)
+	for exp := 1; exp < len(polynomial.Coefficients); exp++ {
+		xbig := big.NewInt(x)
+		fmt.Printf("xbig: %v\n", xbig)
+		p := xbig.Exp(xbig, big.NewInt(int64(exp)), nil)
+		fmt.Printf("p: %v\n", p)
+		//w := big.NewInt(int64(p))
+		r := new(big.Int)
+		r.Mul(polynomial.Coefficients[exp], p)
+		if r.Int64() != polynomial.Coefficients[exp].Int64() {
+			fmt.Printf("r: %v\n", r)
+			fmt.Printf("polynomial.Coefficients[exp]: %v\n", polynomial.Coefficients[exp])
+			os.Exit(1)
+		}
+		acc.Add(acc, r)
+	}
+
+	return acc
+}
+
 // Split a secret into a total number of shares with a minimum number of shares required for secret reconstruction
 func Split(secret *big.Int, polynomial *Polynomial, required int) ([]Share, error) {
 	if required < 0 {
@@ -47,32 +71,49 @@ func Split(secret *big.Int, polynomial *Polynomial, required int) ([]Share, erro
 	if len(polynomial.Coefficients) < required {
 		return nil, ErrRequiredTotal
 	}
-	cs := make([]*big.Int, 0)
-	ss := make([]Share, 0)
-
-	cs = append(cs, secret)
-	for i := 0; i < required; i++ {
-		cs = append(cs, big.NewInt(rand.Int63()))
-	}
+	shares := make([]Share, 0)
 
 	for x := 1; x <= len(polynomial.Coefficients); x++ {
-
-		acc := new(big.Int)
-		acc.Set(cs[0])
-		for exp := 1; exp <= required; exp++ {
-			p := math.Pow(float64(x), float64(exp))
-			w := big.NewInt(int64(p))
-			r := new(big.Int)
-			r.Mul(cs[exp], w)
-			acc.Add(acc, r)
-		}
-
-		ss = append(ss,
+		value := polynomial.Evaluate(int64(x))
+		shares = append(shares,
 			Share{
-				Part:  acc,
-				Piece: x,
+				Y: value,
+				X: x,
 			})
 	}
 
-	return ss, nil
+	return shares, nil
+}
+
+func Join(shares []Share) *big.Int {
+	acc := big.NewInt(0)
+	for j, _ := range shares {
+		val := new(big.Int)
+		val.Set(shares[j].Y)
+
+		acc2 := big.NewInt(1)
+		for m, _ := range shares {
+			if m == j {
+				continue
+			}
+
+			numerator := new(big.Int)
+			numerator.Set(big.NewInt(int64(shares[m].X)))
+
+			denominator := new(big.Int)
+			denominator.Set(numerator)
+			denominator.Sub(denominator, big.NewInt(int64(shares[j].X)))
+
+			final := new(big.Int)
+			final.Set(numerator)
+			final.Div(final, denominator)
+
+			acc2.Mul(acc2, final)
+		}
+
+		val.Mul(val, acc2)
+		acc.Add(acc, val)
+	}
+
+	return acc
 }
--- shamir_test.go
+++ shamir_test.go
@@ -4,23 +4,93 @@ import (
 	"fmt"
 	"math/big"
 	"testing"
+	"testing/quick"
 )
 
-func Test_Split(t *testing.T) {
-	payload := []byte("secret")
-	secret := new(big.Int)
-	secret.SetBytes(payload)
+//func Test_Split(t *testing.T) {
+//	payload := []byte("secret")
+//	secret := new(big.Int)
+//	secret.SetBytes(payload)
 
-	poly, err := NewPolynomial(5)
-	if err != nil {
-		t.Error(err)
+//	poly, err := NewPolynomial(5)
+//	if err != nil {
+//		t.Error(err)
+//	}
+
+//	// 5 total shares with 2 required for secret reconstruction
+//	shares, err := Split(secret, poly, 2)
+//	if err != nil {
+//		t.Error(err)
+//	}
+
+//	fmt.Printf("shares: %v\n", shares)
+//}
+
+//func Test_Evaluate(t *testing.T) {
+//	polynomial := Polynomial{
+
+//		Coefficients: []*big.Int{
+//			big.NewInt(1234),
+//			big.NewInt(166),
+//			big.NewInt(94),
+//		},
+//	}
+
+//	result := polynomial.Evaluate(1)
+//	fmt.Printf("result: %v\n", result)
+//	fmt.Printf("(1234 + 166 + 94): %v\n", (1234 + 166 + 94))
+//}
+
+func Test_QuickCheck(t *testing.T) {
+	counter := 0
+	f := func(x, y, z int64) bool {
+		a := big.NewInt(x)
+		b := big.NewInt(y)
+		c := big.NewInt(z)
+		polynomial := Polynomial{
+			Coefficients: []*big.Int{
+				a, b, c,
+			},
+		}
+
+		result := polynomial.Evaluate(1)
+
+		counter += 1
+		fmt.Printf("counter: %v\n", counter)
+
+		return result == a.Add(a, b).Add(a, c)
 	}
 
-	// 5 total shares with 2 required for secret reconstruction
-	shares, err := Split(secret, poly, 2)
-	if err != nil {
+	if err := quick.Check(f, &quick.Config{
+		MaxCount: 1,
+	}); err != nil {
 		t.Error(err)
 	}
-
-	fmt.Printf("shares: %v\n", shares)
 }
+
+//func Test_Join(t *testing.T) {
+//	payload := []byte("secret")
+//	secret := new(big.Int)
+//	secret.SetBytes(payload)
+
+//	poly, err := NewPolynomial(5)
+//	if err != nil {
+//		t.Error(err)
+//	}
+
+//	// 5 total shares with 2 required for secret reconstruction
+//	shares, err := Split(secret, poly, 2)
+//	if err != nil {
+//		t.Error(err)
+//	}
+
+//	reconstructed := Join(shares)
+
+//	if reconstructed != secret {
+//		fmt.Printf("reconstructed: %v\n", reconstructed)
+//		fmt.Printf("secret: %v\n", secret)
+//		t.FailNow()
+//	}
+
+//	fmt.Printf("shares: %v\n", shares)
+//}
